613d180c280b994d76ae083e5ec39578f2a57376
Krishna752006 <kkc070506@gmail.com>
2025-10-06 20:28:07 +0530

    React Some things

---
+++ code/src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
