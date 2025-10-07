1c5947d09fdd7be065bddd3f46835b5d0ea4782c
LokeshVasireddy <vasireddylokesh3@gmail.com>
2025-10-07 13:27:35 +0530

    React Papalu

---
+++ mentorlink/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import "./index.css";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
